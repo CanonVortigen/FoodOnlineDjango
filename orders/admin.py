1b5f0c5f3c0d7824531b29709889febecbc4414d
CanonVortigen <canonpendragon@outlook.com>
2024-02-05 15:52:18 -0300

    orders and adjustments on PayPal

--- orders/admin.py
+++ orders/admin.py
@@ -4,8 +4,14 @@ from . models import Payment, Order, OrderedFood
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ['user', 'transaction_id', 'payment_method', 'amount', 'status', 'created_at']
 
+class OrderedFoodInline(admin.TabularInline):
+    model = OrderedFood
+    readonly_fields = ('order', 'payment', 'user', 'fooditem', 'quantity', 'price', 'amount')
+    extra = 0
+
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['user', 'payment', 'order_number', 'first_name', 'last_name', 'phone', 'email', 'country', 'state', 'city', 'total', 'payment_method', 'status']
+    list_display = ['user', 'payment', 'order_number', 'name', 'phone', 'email', 'country', 'state', 'city', 'total', 'payment_method', 'status', 'is_ordered']
+    inlines = [OrderedFoodInline]
 
 class OrderedFoodAdmin(admin.ModelAdmin):
     list_display = ['order', 'payment', 'user', 'fooditem', 'quantity', 'price', 'amount']
